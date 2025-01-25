42d36fc48f2a94297a078635e043cae016363ad5
kurilchanin <denis.kurilchanin@gmail.com>
2025-01-25 07:54:17 +0000

    Modified regular expression

--- src/components/CreateNote.jsx
+++ src/components/CreateNote.jsx
@@ -17,7 +17,7 @@ function CreateNote() {
 
   const handleTextChange = (e) => {
     const text = e.target.value;
-    const safePattern = /^[\p{L}\p{N}\s.,!?'"()\-:;/]*$/u;
+    const safePattern = /^[\p{L}\p{N}\s.,!?'"()\-:;±§<>!@#$%^&*()_+=|\"':?/>.<,~`[\]{}]*$/u;
     if (text.length <= MAX_CHARS) {
       setNoteText(text.split('').filter(char => safePattern.test(char)).join(''));
     }
