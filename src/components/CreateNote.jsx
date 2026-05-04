349e6e54ce2a95ac2c818b3522ef33923aee3880
Kurilchanin <denis.kurilchanin@gmail.com>
2026-05-04 13:41:59 +0300

    Increase note character limit from 500 to 1000

--- src/components/CreateNote.jsx
+++ src/components/CreateNote.jsx
@@ -4,7 +4,7 @@ import { FaClipboard, FaShareAlt } from 'react-icons/fa';
 import DOMPurify from 'dompurify';
 import { generateKey, encryptText } from '../utils/crypto';
 
-const MAX_CHARS = 500;
+const MAX_CHARS = 1000;
 const API_URL = import.meta.env.VITE_API_URL;
 
 function CreateNote() {
